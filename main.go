6b235df74b682192cfe3cd4dbf87c05182c57f0b
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-08-27 09:21:32 -0500

    This module is insaly hard

--- main.go
+++ main.go
@@ -1,20 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-
+	fileServer := http.FileServer(http.Dir("."))
+	stripped := http.StripPrefix("/app", fileServer)
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+	apiCfg := &apiConfig{}
 
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(stripped))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1) // increment safely
+		next.ServeHTTP(w, r)      // call the wrapped handler
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	hits := cfg.fileserverHits.Load()
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
 }
