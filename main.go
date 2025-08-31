0b2a55ce4dd3b83bbe7b7f0169199c337a5a5f65
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-08-31 09:56:50 -0500

    I really need more practice :(

--- main.go
+++ main.go
@@ -1,57 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
 )
 
-func main() {
-	const port = "8080"
-
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("."))
-	stripped := http.StripPrefix("/app", fileServer)
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: mux,
-	}
-	apiCfg := &apiConfig{}
-
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(stripped))
-	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
-	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK\n"))
-}
-
 type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1) // increment safely
-		next.ServeHTTP(w, r)      // call the wrapped handler
-	})
-}
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
 
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	hits := cfg.fileserverHits.Load()
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hits: %d\n", hits)
-}
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
+	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
